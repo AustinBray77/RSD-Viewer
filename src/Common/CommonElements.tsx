21ebd2ab84c714cd08a00ba6e9d31fc671e08a43
AustinBray77 <austin.bray77@gmail.com>
2024-06-22 00:14:34 -0400

    UI fixes, code formatting changes

--- src/Common/CommonElements.tsx
+++ src/Common/CommonElements.tsx
@@ -2,116 +2,181 @@ import { useState } from "react";
 import useMousePosition from "../Services/WindowData";
 
 function StandardHomeBox(props: {
-	children: JSX.Element[] | JSX.Element | string;
-	className?: string;
+    children: JSX.Element[] | JSX.Element | string;
+    className?: string;
 }): JSX.Element {
-	return (
-		<div className={"border-2 border-slate-600/[.1] py-3 content-center " + props.className}>
-			{props.children}
-		</div>
-	);
+    return (
+        <div
+            className={
+                "border-2 border-slate-600/[.1] py-3 content-center " +
+                props.className
+            }
+        >
+            {props.children}
+        </div>
+    );
 }
 
-function SmallIcon(props: { src: string, className?: string }): JSX.Element {
-	return <img src={props.src} className={"w-4 h-3 " + props.className} />;
+function SmallIcon(props: { src: string; className?: string }): JSX.Element {
+    return <img src={props.src} className={"w-4 h-3 " + props.className} />;
 }
 
 function DropdownFromList(props: {
-	items: string[];
-	icons: string[];
-	startingIndex: number;
-	onChange: (index: number) => void;
-	className: string;
+    items: string[];
+    icons: string[];
+    startingIndex: number;
+    onChange: (index: number) => void;
+    className: string;
 }): JSX.Element {
-	const [isOpen, setIsOpen] = useState(false);
-	const [currentIndex, setCurrentIndex] = useState(props.startingIndex);
+    const [isOpen, setIsOpen] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(props.startingIndex);
 
-	function getDropdownFromItem(item: string, icon: string, index: number, onClick: (index: number) => void, needsSpacing: boolean, style: string): JSX.Element {
-		return (
-			<div onClick={() => { onClick(index) }} className={"flex items-center " + style}>
-				{
-					needsSpacing ?
-					<div className="w-4 h-3"></div> :
-					""
-				}
-				<SmallIcon src={icon} />
-				&nbsp;
-				<div>
-					{item}
-				</div>
-			</div>
-		);
-	}
+    function getDropdownFromItem(
+        item: string,
+        icon: string,
+        index: number,
+        onClick: (index: number) => void,
+        needsSpacing: boolean,
+        style: string
+    ): JSX.Element {
+        return (
+            <div
+                onClick={() => {
+                    onClick(index);
+                }}
+                className={"flex items-center " + style}
+            >
+                {needsSpacing ? <div className="w-4 h-3"></div> : ""}
+                <SmallIcon src={icon} />
+                &nbsp;
+                <div>{item}</div>
+            </div>
+        );
+    }
 
-	function generateDropdown(): JSX.Element[] {
-		let dropdownItems: JSX.Element[] = [];
+    function generateDropdown(): JSX.Element[] {
+        let dropdownItems: JSX.Element[] = [];
 
-		for (let i = 0; i < props.items.length; i++) {
-			dropdownItems.push(
-				getDropdownFromItem(
-					props.items[i], 
-					props.icons[i], 
-					i, 
-					(index: number) => {
-						if(!isOpen) return;
+        for (let i = 0; i < props.items.length; i++) {
+            dropdownItems.push(
+                getDropdownFromItem(
+                    props.items[i],
+                    props.icons[i],
+                    i,
+                    (index: number) => {
+                        if (!isOpen) return;
 
-						setCurrentIndex(index);
-						setIsOpen(false);
-						props.onChange(index);
-					}, 
-					true, 
-					"border-2 rounded focus:border-slate-600 hover:border-slate-600/[.50] border-slate-700"
-				)
-			);
-		}
+                        setCurrentIndex(index);
+                        setIsOpen(false);
+                        props.onChange(index);
+                    },
+                    true,
+                    "border-2 rounded focus:border-slate-600 hover:border-slate-600/[.50] border-slate-700"
+                )
+            );
+        }
 
-		return dropdownItems;
-	}
+        return dropdownItems;
+    }
 
-	let animationString = isOpen ? "h-20 border-slate-800/[0.50]" : "h-0 border-slate-700";
+    let animationString = isOpen
+        ? "h-20 border-slate-800/[0.50]"
+        : "h-0 border-slate-700";
 
-	return (
-		<div className={props.className}>
-			<div className="flex h-7 focus:outline-none bg-slate-700 border-2 rounded focus:border-slate-600 hover:border-slate-600/[.50] border-slate-700 items-center" onClick={() => { setIsOpen(!isOpen) }}>
-				&nbsp;
-				<img className="w-3 h-3" src="/arrow-down-light.png" />
-				&nbsp;
-				{getDropdownFromItem(props.items[currentIndex], props.icons[currentIndex], currentIndex, (index: number) => {}, false, "")}
-				&nbsp;
-			</div>
-			<div className={"transition-all duration-500 overflow-y-auto fixed bg-slate-700 z-10 rounded border-2 drop-down-scroll " + animationString}> 
-				{generateDropdown()}
-			</div> 
-		</div>
-	);
+    return (
+        <div className={props.className}>
+            <div
+                className="flex h-7 focus:outline-none bg-slate-700 border-2 rounded focus:border-slate-600 hover:border-slate-600/[.50] border-slate-700 items-center"
+                onClick={() => {
+                    setIsOpen(!isOpen);
+                }}
+            >
+                &nbsp;
+                <img className="w-3 h-3" src="/arrow-down-light.png" />
+                &nbsp;
+                {getDropdownFromItem(
+                    props.items[currentIndex],
+                    props.icons[currentIndex],
+                    currentIndex,
+                    (index: number) => {},
+                    false,
+                    ""
+                )}
+                &nbsp;
+            </div>
+            <div
+                className={
+                    "transition-all duration-500 overflow-y-auto fixed bg-slate-700 z-10 rounded border-2 drop-down-scroll " +
+                    animationString
+                }
+            >
+                {generateDropdown()}
+            </div>
+        </div>
+    );
 }
 
-function Title(props: { children: string, className?: string }): JSX.Element {
-	return <StandardHomeBox>
-			<h3 className={"flex justify-center text-2xl " + props.className}>{props.children}</h3>
-		</StandardHomeBox>
+function Title(props: { children: string; className?: string }): JSX.Element {
+    return (
+        <StandardHomeBox>
+            <h3 className={"flex justify-center text-2xl " + props.className}>
+                {props.children}
+            </h3>
+        </StandardHomeBox>
+    );
 }
 
-function ToolTip(props: { children: string, offset?: [number, number] }): JSX.Element {
-	const mousePos = useMousePosition();
+function ToolTip(props: {
+    children: string;
+    offset?: [number, number];
+}): JSX.Element {
+    const mousePos = useMousePosition();
 
-	//console.log("Width %d, Height %d", width, height);
-	//console.log("Mouse Pos: %d, %d", mousePos.Value.x, mousePos.Value.y);
+    //console.log("Width %d, Height %d", width, height);
+    //console.log("Mouse Pos: %d, %d", mousePos.Value.x, mousePos.Value.y);
 
-	const translationString = "translate(" + (mousePos.Value.x - props.offset![0] + 10) + "px," + (mousePos.Value.y- props.offset![1] + 30) + "px)";
-	
-	return <label className="fixed z-50 bg-slate-700 z-10 border-2 rounded border-slate-800/[0.50] px-1 py-1" style={{transform: translationString, zIndex: 1000 }}>
-		{props.children}
-	</label>
+    const translationString =
+        "translate(" +
+        (mousePos.Value.x - props.offset![0] + 10) +
+        "px," +
+        (mousePos.Value.y - props.offset![1] + 30) +
+        "px)";
+
+    return (
+        <label
+            className="fixed z-50 bg-slate-700 z-10 border-2 rounded border-slate-800/[0.50] px-1 py-1"
+            style={{ transform: translationString, zIndex: 1000 }}
+        >
+            {props.children}
+        </label>
+    );
 }
 
-function RetractArrow(props: { className?: string, subClassName?:string, onClick: () => void }): JSX.Element {
-	return <div className={"fixed bg-slate-700 py-2 px-3 rounded-b " + props.className} onClick={props.onClick}>
-			<img 
-            	className={"w-4 h-3 " + props.subClassName} 
-            	src="/arrow-down-light.png"
-        	/>
-		</div>
+function RetractArrow(props: {
+    className?: string;
+    subClassName?: string;
+    onClick: () => void;
+}): JSX.Element {
+    return (
+        <div
+            className={
+                "fixed bg-slate-700 py-2 px-3 rounded-b " + props.className
+            }
+            onClick={props.onClick}
+        >
+            <img
+                className={"w-4 h-3 " + props.subClassName}
+                src="/arrow-down-light.png"
+            />
+        </div>
+    );
 }
 
-export { StandardHomeBox, DropdownFromList, Title, ToolTip, RetractArrow, SmallIcon };
+export {
+    StandardHomeBox,
+    DropdownFromList,
+    Title,
+    ToolTip,
+    RetractArrow,
+    SmallIcon,
+};
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,131 +1,203 @@
 import React, { useMemo, useState } from "react";
-import { OptionsColumn, CopyPasswordDialog, ChangePasswordDialog, RemovePasswordDialog, CopyPasswordButton, ChangePasswordButton, RemovePasswordButton } from "./OptionsColumn";
-import AccountColumn from "./AccountColumn";
+import {
+    CopyPasswordDialog,
+    ChangePasswordDialog,
+    RemovePasswordDialog,
+    CopyPasswordButton,
+    ChangePasswordButton,
+    RemovePasswordButton,
+} from "./OptionsColumn";
 import { StatePair, useStatePair } from "../StatePair";
 import { AccountData } from "../Services/AccountData";
-import SideButtons from "./SideButtons";
 import { SmallIcon, StandardHomeBox, Title } from "../Common/CommonElements";
 
 enum ShowHomeDialog {
-	None,
-	CopyPassword,
-	ChangePassword,
-	RemovePassword
+    None,
+    CopyPassword,
+    ChangePassword,
+    RemovePassword,
 }
 
 type HomeState = {
-	data: AccountData[]
-	setData: (val: AccountData[]) => void
-	dialog: StatePair<ShowHomeDialog>
-	selectedAccount: StatePair<number>
+    data: AccountData[];
+    setData: (val: AccountData[]) => void;
+    dialog: StatePair<ShowHomeDialog>;
+    selectedAccount: StatePair<number>;
+};
+
+function RowButtons(props: { isHovering: boolean }): JSX.Element {
+    let opacity = props.isHovering ? "opacity-50" : "opacity-0";
+    let classString =
+        "self-center transition-opacity duration-300 ease-in-out " + opacity;
+
+    return (
+        <div
+            className="w-5 min-w-fit p-3 place-content-evenly"
+            onClick={() => {}}
+        >
+            <div className="grid grid-cols-1 grid-rows-2 place-content-evenly h-full">
+                <SmallIcon
+                    src="/arrow-down-light.png"
+                    className={classString + " rotate-180"}
+                />
+                <SmallIcon
+                    src="/arrow-down-light.png"
+                    className={classString}
+                />
+            </div>
+        </div>
+    );
 }
 
-const GenerateAccounts = (data: AccountData[]) => {
-	console.log(data);
-	
-	return data.map((account) => {
-		console.log(account);
+function OptionsButtons(props: {
+    account: AccountData;
+    index: number;
+    state: HomeState;
+}): JSX.Element {
+    const { account, index, state } = props;
 
-		return <li>
-			
-		</li>
-	})
+    return (
+        <StandardHomeBox className="flex justify-center w-1/3 min-w-96">
+            <CopyPasswordButton text={account.Password} dialog={state.dialog} />
+            <ChangePasswordButton state={state} accountIndex={index} />
+            <RemovePasswordButton state={state} accountIndex={index} />
+        </StandardHomeBox>
+    );
 }
+
+function AccountDisplay(props: { account: AccountData }): JSX.Element {
+    return (
+        <StandardHomeBox className="w-2/3 min-w-96">
+            <div className="px-5 text-lg leading-[3.5rem]">
+                {props.account.Name}
+            </div>
+        </StandardHomeBox>
+    );
+}
+
+const HomeRow = (props: {
+    account: AccountData;
+    index: number;
+    state: HomeState;
+    length: number;
+}) => {
+    const { account, index, state, length } = props;
+    const isHovering = useStatePair(false);
+
+    let borderString = "border-x-2 border-slate-600/[.1]";
+
+    if (isHovering.Value) {
+        borderString += " border-x-slate-400";
+    }
+
+    if (index == length - 1) {
+        borderString += " border-b-2";
+    }
+
+    return (
+        <div
+            className="flex place-content-center"
+            onMouseEnter={() => isHovering.Set(true)}
+            onMouseLeave={() => isHovering.Set(false)}
+        >
+            <RowButtons isHovering={isHovering.Value} />
+            <div
+                className={
+                    "w-[90vw] min-w-fit transition-border duration-300 " +
+                    borderString
+                }
+            >
+                <div className="flex">
+                    <AccountDisplay account={account} />
+                    <OptionsButtons
+                        account={account}
+                        index={index}
+                        state={state}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const GenerateRows = (state: HomeState) => {
-	const { data, dialog } = state;
-	
-	return data.map((account, i) => {
-		if(account.IsSpecial) return <></>;
+    const { data } = state;
 
-		let x = i;
+    return data.map((account, index) => {
+        if (account.IsSpecial) return <></>;
 
-		return <div className="flex">
-			<div className="w-5 min-w-fit p-3 content-center" onClick={() => {}}>
-                <SmallIcon src="/arrow-down-light.png" className="rotate-180 opacity-50 mb-4" />
-                <SmallIcon src="/arrow-down-light.png" className="opacity-50" />
-            </div>
-			<div className="w-[85vw] min-w-fit border-2 border-slate-600/[.1]">
-				<div className="flex">
-					<StandardHomeBox className="w-2/3 min-w-96">
-						<div className="px-5">
-							{account.Name}
-						</div>
-					</StandardHomeBox>
-					<StandardHomeBox className="flex justify-center w-1/3 min-w-96">
-						<CopyPasswordButton
-							text={account.Password}
-							dialog={dialog}
-						/>
-						<ChangePasswordButton state={state} accountIndex={x} />
-						<RemovePasswordButton state={state} accountIndex={x} />
-					</StandardHomeBox>
-				</div>
-			</div>
-		</div>
-	});
+        return (
+            <HomeRow
+                account={account}
+                index={index}
+                state={state}
+                length={data.length}
+            />
+        );
+    });
 };
 
 function Home(props: {
-	data: AccountData[]
-	setData: (val: AccountData[]) => void
+    data: AccountData[];
+    setData: (val: AccountData[]) => void;
 }): JSX.Element {
-	const dialog = useStatePair(ShowHomeDialog.None);
+    const dialog = useStatePair(ShowHomeDialog.None);
 
-	let filteredData = props.data.filter(account => !account.IsSpecial);
+    let filteredData = props.data.filter((account) => !account.IsSpecial);
 
-	let state = {
-		data: props.data,
-		setData: props.setData,
-		dialog: dialog,
-		selectedAccount: useStatePair(-1)
-	}
+    let state = {
+        data: props.data,
+        setData: props.setData,
+        dialog: dialog,
+        selectedAccount: useStatePair(-1),
+    };
 
-	if (filteredData.length == 0) {
-		return <div className="p-8 text-slate-100 overflow-y-auto content-center flex justify-center">
-				<h1 className="text-5xl">No Accounts Yet</h1>
-			</div>
-	}
+    if (filteredData.length == 0) {
+        return (
+            <div className="p-8 text-slate-100 overflow-y-auto content-center flex justify-center">
+                <h1 className="text-5xl">No Accounts Yet</h1>
+            </div>
+        );
+    }
 
-	let rows = GenerateRows(state);
+    let rows = GenerateRows(state);
 
-	rows.unshift(
-		<div className="flex">
-			<div className="w-5 min-w-fit p-3">
-				<SmallIcon src="/arrow-down-light.png" className="rotate-180 opacity-0 mb-4" />
-				<SmallIcon src="/arrow-down-light.png" className="opacity-0" />
-			</div>
-			<div className="w-[85vw] min-w-fit border-2 border-slate-600/[.1]">
-				<div className="flex">
-					<div className="w-2/3 min-w-96">
-						<Title className="flex justify-center">
-							Accounts
-						</Title>
-					</div>
-					<div className="w-1/3 min-w-96">
-						<Title className="flex justify-center">
-							Options
-						</Title>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    rows.unshift(
+        <div className="flex place-content-center">
+            <div className="w-5 min-w-fit p-3">
+                <SmallIcon
+                    src="/arrow-down-light.png"
+                    className="rotate-180 opacity-0 mb-4"
+                />
+                <SmallIcon src="/arrow-down-light.png" className="opacity-0" />
+            </div>
+            <div className="w-[90vw] min-w-fit border-x-2 border-t-2 border-slate-600/[.1]">
+                <div className="flex">
+                    <div className="w-2/3 min-w-96">
+                        <Title className="flex justify-center leading-[3rem]">
+                            Accounts
+                        </Title>
+                    </div>
+                    <div className="w-1/3 min-w-96">
+                        <Title className="flex justify-center leading-[3rem]">
+                            Options
+                        </Title>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 
-	return (
-		<div className="text-slate-100 overflow-y-auto h-[100vh]">
-			<div
-				className={
-					"grid grid-cols-1 grid-flow-row w-screen p-8"
-				}
-			>
-				{rows}
-			</div>
-			<CopyPasswordDialog state={state} />
-			<ChangePasswordDialog state={state} />
-			<RemovePasswordDialog state={state} />
-		</div>
-	);
+    return (
+        <div className="text-slate-100 overflow-y-auto h-screen">
+            <div className={"grid grid-cols-1 grid-flow-row w-[95vw] p-8"}>
+                {rows}
+            </div>
+            <CopyPasswordDialog state={state} />
+            <ChangePasswordDialog state={state} />
+            <RemovePasswordDialog state={state} />
+        </div>
+    );
 }
 
 export { Home, HomeState, ShowHomeDialog };
