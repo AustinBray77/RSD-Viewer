a961ca3a3710ffdc0bacd4dcf1f0786cb886d03b
AustinBray77 <austin.bray77@gmail.com>
2024-07-25 16:34:51 -0400

    Unified versions, added version marker on home screen updated updater endpoint url other minor fixes

--- src/Toolbar/ToolbarHeader.tsx
+++ src/Toolbar/ToolbarHeader.tsx
@@ -4,70 +4,85 @@ import { Get2FACode } from "../Services/TwoFactorAuth";
 import { ShowDialog, ToolbarState } from "./Toolbar";
 
 function HeaderButton(props: {
-	onClick: () => void;
-	title: string;
-	className?: string;
+    onClick: () => void;
+    title: string;
+    className?: string;
 }): JSX.Element {
-	return (
-		<div className={"inline-flex border-1 border-slate-700 content-end " + props.className!}>
-			<button className="text-xl p-3" onClick={props.onClick}>
-				{props.title}
-			</button>
-		</div>
-	);
+    return (
+        <div
+            className={
+                "inline-flex border-1 border-slate-700 content-end " +
+                props.className!
+            }
+        >
+            <button className="text-xl p-3" onClick={props.onClick}>
+                {props.title}
+            </button>
+        </div>
+    );
 }
 
-function ToolbarHeader(props: { state: ToolbarState, appState: AppState, has2FA: boolean }): JSX.Element {
+function ToolbarHeader(props: {
+    state: ToolbarState;
+    appState: AppState;
+    has2FA: boolean;
+}): JSX.Element {
     const { showDialog, phoneNumber, tfaCode } = props.state;
 
     const Handle2FA = () => {
-        if(props.has2FA) {
+        if (props.has2FA) {
             Get2FACode(phoneNumber.Value, props.appState.isLoading)
                 .then(async (res: string) => {
                     tfaCode.Set(res);
                     showDialog.Set(ShowDialog.Verify2FA);
                 })
                 .catch((err: any) => {
-                    props.appState.error.Set("2FA Error: " + err as string);
+                    props.appState.error.Set(("2FA Error: " + err) as string);
                 });
         } else {
             showDialog.Set(ShowDialog.AddPhoneNumber);
         }
-    }
+    };
 
-    return <div className="px-8 py-5 bg-slate-700 flex content-center flex-wrap">
-        <h1 className="inline-flex p-3 text-2xl" id="Title">
-            RSD Password Manager
-        </h1>
-        <HeaderButton
-            onClick={() => {
-                showDialog.Set(ShowDialog.AddAccount);
-            }}
-            title="Add Account"
-        />
-        <HeaderButton
-            onClick={() => {
-                showDialog.Set(ShowDialog.GeneratePassword);
-            }}
-            title="Generate Password"
-        />
-        <HeaderButton
-            onClick={() => {
-                ExportFile(props.appState.error.Set);
-            }}
-            title="Export Save File"
-        />
-        <HeaderButton
-            onClick={() => {
-                showDialog.Set(ShowDialog.ImportFile);
-            }}
-            title="Import Save File"
-        />
-        <HeaderButton
-            onClick={Handle2FA}
-            title={props.has2FA ? "Remove 2FA" : "Add 2FA"}
-        />
-    </div>;
+    return (
+        <div className="px-8 py-5 bg-slate-700 flex content-center flex-wrap">
+            <div className="block p-3" id="Title">
+                <h1 className="text-2xl">RSD Password Manager</h1>
+                <br />
+                <div className="text-slate-500 text-sm">
+                    By: Austin Bray | v1.0.0
+                </div>
+            </div>
+            <HeaderButton
+                onClick={() => {
+                    showDialog.Set(ShowDialog.AddAccount);
+                }}
+                title="Add Account"
+            />
+            <HeaderButton
+                onClick={() => {
+                    showDialog.Set(ShowDialog.GeneratePassword);
+                }}
+                title="Generate Password"
+            />
+            <HeaderButton
+                onClick={() => {
+                    ExportFile(props.appState.error.Set);
+                }}
+                title="Export Save File"
+            />
+            <HeaderButton
+                onClick={() => {
+                    showDialog.Set(ShowDialog.ImportFile);
+                }}
+                title="Import Save File"
+            />
+            <HeaderButton
+                onClick={Handle2FA}
+                title={props.has2FA ? "Remove 2FA" : "Add 2FA"}
+            />
+        </div>
+    );
 }
 
 export default ToolbarHeader;
