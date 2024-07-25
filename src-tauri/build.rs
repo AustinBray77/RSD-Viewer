d03d9f73994717648318f8237ec36a197c90580b
AustinBray77 <austin.bray77@gmail.com>
2024-07-25 12:14:17 -0400

    Fixed build errors, moved windows manifest to seperate file

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,33 @@
 //extern crate embed_resource;
 
 mod src;
+use std::{fs::{self}, io::Read};
+
 use src::environment;
+
+fn build_windows() {
+    let mut windows = tauri_build::WindowsAttributes::new();
+
+    let mut manifest_file = 
+        fs::OpenOptions::new()
+        .read(true)
+        .open("./windows_manifest.xml")
+        .expect("Error opening manifest file");
+
+    let mut manifest_str = String::new();
+    
+    manifest_file.read_to_string(&mut manifest_str).expect("Error parsing manifest file");
+
+    windows = windows.app_manifest(manifest_str);
+
+    let attributes = tauri_build::Attributes::new().windows_attributes(windows);
+
+    tauri_build::try_build(attributes).expect("failed to build");
+}
+
+fn build_linux() {
+    tauri_build::build();
+}
 
 fn main() {
     let platform = environment::ENV_VARS.BUILD_PLATFORM.clone();
@@ -10,28 +36,10 @@ fn main() {
 
     match platform {
         environment::BuildPlatform::Windows => {
-            let mut windows = tauri_build::WindowsAttributes::new();
-
-        windows = windows.app_manifest(
-           r#"
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-      <security>
-          <requestedPrivileges>
-              <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-          </requestedPrivileges>
-      </security>
-  </trustInfo>
-</assembly>
-"#,
-        );
-
-        let attributes = tauri_build::Attributes::new().windows_attributes(windows);
-
-        tauri_build::try_build(attributes).expect("failed to build");
+            build_windows();
         },
         environment::BuildPlatform::Linux => {
-            tauri_build::build();
+            build_linux();
         }
         
     }
